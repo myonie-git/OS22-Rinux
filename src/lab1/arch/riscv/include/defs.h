f45ec24110a084244095810c9111b9ea02073388
myonie <myonie.0724@gmail.com>
2023-02-28 23:23:56 +0800

    completed

--- src/lab1/arch/riscv/include/defs.h
+++ src/lab1/arch/riscv/include/defs.h
@@ -3,12 +3,15 @@
 
 #include "types.h"
 
-#define csr_read(csr)                       \
+#define csr_read(csr,val)                       \
 ({                                          \
     register uint64 __v;                    \
-    /* unimplemented */                     \
-    __v;                                    \
-})
+    asm volatile("csrr %[__v]," #csr        \
+     		   :[__v] "=r" (__v)        \
+    		   :                        \
+                   : "memory");             \
+     val = __v;                             \
+})                                          \
 
 #define csr_write(csr, val)                         \
 ({                                                  \
--- src/lab1/init/main.c
+++ src/lab1/init/main.c
@@ -1,11 +1,27 @@
 #include "print.h"
 #include "sbi.h"
+#include "defs.h"
 
 extern void test();
 
 int start_kernel() {
+    int val = 0;
+    /*
+    puts("sscratch : ");
+    csr_read(sscratch, val);
+    puti(val);
+    puts("\n");
+    val = 1;
+    csr_write(sscratch, val);
+    puts("Overwrite the value of sscratch : ");
+    puti(val);
+    puts("\nsscratch : ");
+    csr_read(sscratch, val);
+    puti(val);
+    puts("\n");
+    */
     puti(2022);
-    puts(" Hello RISC-V\n");
+    puts(" Hello RISC-V 3190105197\n");
 
     test(); // DO NOT DELETE !!!
 
